eb27248b6afe5ef8d63e38d5491d9337a21a7989
Hteam121 <Shaikhatim3@gmail.com>
2023-05-07 14:11:52 -0500

    Fixed output error and changed up the code to work with multiple flights

--- src/Graph.java
+++ src/Graph.java
@@ -9,31 +9,44 @@ public class Graph {
         adjacencyList = new HashMap<>();
     }
 
-    //Create a way to add a flight to each linked list within the map
     public void addFlight(Flight flight) {
         String start = flight.getStart();
+        String end = flight.getEnd();
 
+        //add flight in the original direction
+        addFlightToList(start, end, flight);
+
+        //Add flight in the opposite direction
+        Flight oppositeFlight = new Flight(end, start, flight.getCost(), flight.getTime());
+        addFlightToList(end, start, oppositeFlight);
+    }
+
+    private void addFlightToList(String start, String end, Flight flight) {
         LinkedList<Flight> flightsFromStart = adjacencyList.get(start);
         if (flightsFromStart == null) {     //checks to see if start city exists
             flightsFromStart = new LinkedList<>();
             flightsFromStart.add(flight);
             adjacencyList.put(start, flightsFromStart);
+
         } else {    //adds flight if it does exist
             adjacencyList.get(start).add(flight);
         }
     }
 
     //Find all paths method
-    private void findPaths(String start, String end, List<Flight> currentPath, List<List<Flight>> allPaths, Stack<String> sortedCities) {
+    private void findPaths(String start, String end, List<Flight> currentPath, List<List<Flight>> allPaths, Set<String> visited) {
         if (start.equals(end)) {
             allPaths.add(new ArrayList<>(currentPath));
             return;
         }
 
+        visited.add(start); //make sure it doesn't keep looping through since its an undirected graph
+
         LinkedList<Flight> neighbors = adjacencyList.get(start);
 
-        //If no flights are connected to the start city then return nothing
+        //If no flights are connected to the start city, then return nothing
         if (neighbors == null) {
+            visited.remove(start);
             return;
         }
 
@@ -41,23 +54,26 @@ public class Graph {
             Flight flight = neighbors.get(i);
             String nextCity = flight.getEnd();
 
-            //Check if the next city is valid based on the order of the cities
-            if (sortedCities.indexOf(start) < sortedCities.indexOf(nextCity)) {
+            if (!visited.contains(nextCity)) {
                 currentPath.add(flight);
 
-                //Basically this will recursively search for paths from the cities linked through recursion
-                findPaths(nextCity, end, currentPath, allPaths, sortedCities);
+                //Basically, this will recursively search for paths from the cities linked through recursion
+                findPaths(nextCity, end, currentPath, allPaths, visited);
                 currentPath.remove(currentPath.size() - 1);
             }
         }
+
+        visited.remove(start);
     }
+
+
 
     //Topological sort method
     public Stack<String> topologicalSort() {
         Stack<String> stack = new Stack<>();
         Set<String> visited = new HashSet<>();
 
-        List<String> cities = new LinkedList<>(adjacencyList.keySet());
+        LinkedList<String> cities = new LinkedList<>(adjacencyList.keySet());
 
         //Iterate through all cities and visit them if they haven't been visited
         for (int i = 0; i < cities.size(); i++) {
@@ -94,15 +110,12 @@ public class Graph {
         stack.push(city);
     }
 
-    public List<String> getFlightPlans(String start, String end, char flag) {
+    public List<String> getFlightPlans(String start, String end, char flag, int flightNumber) {
         List<String> result = new ArrayList<>();
         List<List<Flight>> allPaths = new ArrayList<>();
 
-        //Perform the topological sort
-        Stack<String> sortedCities = topologicalSort();
-
-        //Find paths using topological sort
-        findPaths(start, end, new ArrayList<>(), allPaths, sortedCities);
+        //Find paths without using the sorted cities list
+        findPaths(start, end, new ArrayList<>(), allPaths, new HashSet<>());
 
         //Sort the paths based on the flag input
         if (flag == 'T') {
@@ -111,21 +124,24 @@ public class Graph {
             costSorter(allPaths);
         }
 
+        //Add the flight number and cities to the result
+        result.add("Flight " + flightNumber + ": " + start + ", " + end + " (" + (flag == 'T' ? "Time" : "Cost") + ")");
+
         //Iterate through all paths and generate the result strings
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < allPaths.size(); i++) {
             List<Flight> path = allPaths.get(i);
-            String ans = "Path: ";
+            String ans = "Path " + (i + 1) + ": ";
             double totalCost = 0;
             int totalTime = 0;
 
             for (int j = 0; j < path.size(); j++) {
                 Flight flight = path.get(j);
-                ans += flight.getStart() + " -> " + flight.getEnd() + ", ";
+                ans += flight.getStart() + " -> " + flight.getEnd() + (j < path.size() - 1 ? " -> " : ". ");
                 totalCost += flight.getCost();
                 totalTime += flight.getTime();
             }
 
-            ans += "Time: " + totalTime + ", Cost: " + String.format("%.2f", totalCost);
+            ans += "Time: " + totalTime + " Cost: " + String.format("%.2f", totalCost);
             result.add(ans);
         }
 
--- src/flightPlan.java
+++ src/flightPlan.java
@@ -2,24 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class flightPlan {
+    private static final String FLIGHT_DATA_FILE = "/Users/hatim/Downloads/CS/Assignments/AVL Trees/FlightPlanner/src/input.txt";
+    private static final String PATHS_TO_CALCULATE_FILE = "/Users/hatim/Downloads/CS/Assignments/AVL Trees/FlightPlanner/src/PathsToCalculateFile.txt";
+    private static final String OUTPUT_FILE = "/Users/hatim/Downloads/CS/Assignments/AVL Trees/FlightPlanner/src/output.txt";
+
     public static void main(String[] args) {
 
-        //checks to see if the arguments are correct within the command line
-        if (args.length != 3) {
-            System.out.println("Usage: java flightPlan <FlightDataFile> <PathsToCalculateFile> <OutputFile>");
-            System.exit(1);
-        }
-
-        //stores to corresponding variables based on given arguments
-        String flightDataFile = args[0];
-        String pathsToCalculateFile = args[1];
-        String outputFile = args[2];
-
-        //Create a new Graph to store data
-        Graph graph = new Graph();
-
-        // Read the flight data file
-        try (BufferedReader br = new BufferedReader(new FileReader(flightDataFile))) {
+        //Read the flight data file
+        ArrayList<Flight> flights = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(FLIGHT_DATA_FILE))) {
             int numberOfFlights = Integer.parseInt(br.readLine());
 
             for (int i = 0; i < numberOfFlights; i++) {
@@ -30,35 +21,61 @@ public class flightPlan {
                 double cost = Double.parseDouble(splitArray[2]);
                 int time = Integer.parseInt(splitArray[3]);
 
-                graph.addFlight(new Flight(start, end, cost, time));
+                flights.add(new Flight(start, end, cost, time));
             }
         } catch (IOException e) { //in case file not found, or line errors
             e.printStackTrace();
         }
 
-        // Read the paths to calculate file
-        List<String> flightPlans = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(pathsToCalculateFile))) {
-            int numberOfPaths = Integer.parseInt(br.readLine());
+        //Read the paths to calculate file
+        ArrayList<String> flightPlans = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(PATHS_TO_CALCULATE_FILE))) {
+            int numOfFlights = Integer.parseInt(br.readLine());
 
-            for (int i = 0; i < numberOfPaths; i++) {
+            for (int i = 0; i < numOfFlights; i++) {
+                Graph graph = new Graph(); //Create a new graph instance for each flight plan
+
+                //Add flights to the graph
+                for(int j = 0; j < flights.size(); j++) {
+                    graph.addFlight(flights.get(j));
+                }
+
                 String[] splitArray = br.readLine().split("\\|");
                 String start = splitArray[0];
                 String end = splitArray[1];
                 char flag = splitArray[2].charAt(0);
 
-                flightPlans.addAll(graph.getFlightPlans(start, end, flag));
+                flightPlans.addAll(graph.getFlightPlans(start, end, flag, i + 1));
+//                System.out.println(flightPlans);
             }
         } catch (IOException e) {  //in case file not found, or line errors
             e.printStackTrace();
         }
 
         // Write the output to a file
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
-            for (String plan : flightPlans) {
-                bw.write(plan);
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_FILE))) {
+//            System.out.println("Generated flight plans: " + flightPlans); //writes onto the console for extra checking
+
+            if (!flightPlans.isEmpty()) {
+                int numOfFlights = flights.size(); //assuming the same number of flights as in the FLIGHT_DATA_FILE
+                int counter = 0;
+
+                for (int i = 0; i < flightPlans.size(); i++) {
+                    if (i > 0 && (i + 1) % numOfFlights == 0 && i != flightPlans.size() - 1) {
+                        bw.newLine(); //Add an empty line between flight plans
+                        System.out.println();
+                    }
+
+                    bw.write(flightPlans.get(i));
+                    System.out.println(flightPlans.get(i)); //writes onto the console for extra checking
+
+                    bw.newLine();
+                }
+            } else {
+                bw.write("NO FLIGHT PLANS FOUND, SORRY");
                 bw.newLine();
             }
+
         } catch (IOException e) {  //in case file not found, or line errors
             e.printStackTrace();
         }
